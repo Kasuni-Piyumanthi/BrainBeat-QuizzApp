f8961aaec64c5f22c110eda4eec42e2d44e4dc55
Kasuni-Piyumanthi <kpiyumanthi@gmail.com>
2025-12-23 11:17:45 +0530

    changed

--- src/Components/Quiz/Quiz.jsx
+++ src/Components/Quiz/Quiz.jsx
@@ -3,8 +3,18 @@ import './Quiz.css'
 
 const Quiz = () => {
   return (
-    <div className='container'>
-        <h1></h1>
+    <div className="container">
+        <h1>Quiz App</h1>
+        <hr />
+        <h2>Which device is required for the Internet Connection?</h2>
+        <ul>
+            <li>Router</li>
+            <li>Modem</li>
+            <li>LAN Cable</li>
+            <li>Pen Drive</li>
+        </ul>
+        <button>Next</button>
+        <div className="index">1 of 5 Questions</div>
     </div>
   )
 }
